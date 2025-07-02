500b7bc7880c78e71b2291e7576638b6cde26c74
Farheen Shaikh <farheen040407@gmail.com>
2025-07-02 20:52:43 +0530

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,10 @@
 #A simple console-based Tic Tac Toe game for 2 players, developed using basic programming concepts like variables, datatypes, conditional & looping statements, functions
 
+import os
+import time
+
+grid_value = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def place_symbol(grid_map,symb,grid_value):
     """
         Argument (as String): grid map, symbol of player, value of grids
@@ -11,6 +16,16 @@ def place_symbol(grid_map,symb,grid_value):
         Returns: Value of grids
     """
     print(grid_map)
+
+    #print(grid_values)
+    print(f'''
+         {grid_value[0]}  |  {grid_value[1]}  |  {grid_value[2]}
+       _____ _____ _____
+         {grid_value[3]}  |  {grid_value[4]}  |  {grid_value[5]}
+       _____ _____ _____
+         {grid_value[6]}  |  {grid_value[7]}  |  {grid_value[8]}
+    ''')
+
     user_in = input("Enter position (1-9): ")
     if user_in == "1" and grid_value[0] == " ":
         grid_value[0] = symb
@@ -32,6 +47,8 @@ def place_symbol(grid_map,symb,grid_value):
         grid_value[8] = symb
     else:
         print("Invalid position!")
+
+    #print(grid_values)
     print(f'''
          {grid_value[0]}  |  {grid_value[1]}  |  {grid_value[2]}
        _____ _____ _____
@@ -39,15 +56,16 @@ def place_symbol(grid_map,symb,grid_value):
        _____ _____ _____
          {grid_value[6]}  |  {grid_value[7]}  |  {grid_value[8]}
     ''')
+
     return grid_value
 
-def win(grid_value,player1, player2):
+def win(grid_value):
     """
-        Argument (as String): value of grids, names of players
+        Argument (as String): value of grids
 
         Input : None
 
-        It compares grid values and grid value to symbol(X/O), if they satisfy winning condition or tie condition then it displays the corresponding result
+        It compares grid values and grid value to symbol(X/O), if they satisfy winning condition or tie condition then it returns the corresponding result
         
         Winning condition: Same symbol in the following grid position combinations
             - 012, 345, 678 (horizontal)
@@ -55,72 +73,66 @@ def win(grid_value,player1, player2):
             - 048, 246 (diagonal)
         Tie condition: No " " symbol in grid and no same symbol in above grid position combination
 
-        Returns: True if one of the above condition is satisfied
+        Returns: "X" if above condition is satisfied & symbol is "X"
+                 "O" if above condition is satisfied & symbol is not "X", i.e. "O"
+                 "True" if Tie condition is satisfied
                  False if NO condition is satisfied
     """
     # 012, 345, 678 (horizontal)
     if grid_value[0] == grid_value[1] == grid_value[2] != " ": #1
         if grid_value[0] == "X":
-            print(f"\n\t{player1} is winner!!")
+            return "X"
         else:
-            print(f"\n\t{player2} is winner!!")
-        return True
+            return "O"
 
     elif grid_value[3] == grid_value[4] == grid_value[5] != " ": #2
         if grid_value[3] == "X":
-            print(f"\n{player1} is winner!!")
+            return "X"
         else:
-            print(f"\n{player2} is winner!!")
-        return True
+            return "O"
 
     elif grid_value[6] == grid_value[7] == grid_value[8] != " ": #3
         if grid_value[6] == "X":
-            print(f"\n{player1} is winner!!")
+            return "X"
         else:
-            print(f"\n{player2} is winner!!")
-        return True
+            return "O"
     
     # 036, 147, 258 (vertical)
     if grid_value[0] == grid_value[3] == grid_value[6] != " ": #1
         if grid_value[0] == "X":
-            print(f"\n{player1} is winner!!")
+            return "X"
         else:
-            print(f"\n{player2} is winner!!")
-        return True
+            return "O"
 
     elif grid_value[1] == grid_value[4] == grid_value[7] != " ": #2
         if grid_value[1] == "X":
-            print(f"\n{player1} is winner!!")
+            return "X"
         else:
-            print(f"\n{player2} is winner!!")
-        return True
+            return "O"
 
     elif grid_value[2] == grid_value[5] == grid_value[8] != " ": #3
         if grid_value[2] == "X":
-            print(f"\n{player1} is winner!!")
+            return "X"
         else:
-            print(f"\n{player2} is winner!!")
-        return True
+            return "O"
 
     # 048, 246 (diagonal)
     if grid_value[0] == grid_value[4] == grid_value[8] != " ": #1
         if grid_value[0] == "X":
-            print(f"\n{player1} is winner!!")
+            return "X"
         else:
-            print(f"\n{player2} is winner!!")
-        return True
+            return "O"
 
     elif grid_value[2] == grid_value[4] == grid_value[6] != " ": #2
         if grid_value[2] == "X":
-            print(f"\n{player1} is winner!!")
+            return "X"
         else:
-            print(f"\n{player2} is winner!!")
-        return True
+            return "O"
     
     #TIE
     if " " not in grid_value and state_win_tie == False:
-        print("\nIt's a Tie!")
-        return True
+        #print("\nIt's a Tie!")
+        return "True"
 
     return False #if no winner
 
@@ -143,18 +155,47 @@ _____ _____ _____
 
 print(welcome_mssg)
 
-grid_value = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
 state_win_tie = False
+
+color_list = ["01","02","03","04","05","06"] #blue, green, cyan/aqua, red, magenta/purple, yellow
 
 player1 = input("Enter player 1 name: ")
 player2 = input("Enter player 2 name: ")
 
 while not state_win_tie:
     for name,symbol in zip([player1,player2], ["X","O"]):
+        if symbol == "X":
+            os.system("color 01") #Player X -> blue
+        else:
+            os.system("color 02") #Player O -> green
+
         print(f"\n\n{name}'s turn: (Symbol: {symbol})")
         place_symbol(grid_map,symbol,grid_value)
-        state_win_tie = win(grid_value, player1, player2)
+
+        time.sleep(1)
+        os.system("cls") 
         
-        if state_win_tie == True:
+        state_win_tie = win(grid_value)
+        
+        if state_win_tie == "X": 
+            game_end = f"\n\t\t*** {player1} is winner!! ***"
+        elif state_win_tie == "O":
+            game_end = f"\n\t\t*** {player2} is winner!! ***"
+        elif state_win_tie == "True":
+            game_end = f"\n\t\t*** It is a Tie!! ***"
+
+        if state_win_tie or state_win_tie == "True": #if state is X/O(True) or "True"(Tie)
+            for color in color_list:
+                os.system(f"color {color}")
+                print(f'''
+                      {grid_value[0]}  |  {grid_value[1]}  |  {grid_value[2]}
+                    _____ _____ _____
+                      {grid_value[3]}  |  {grid_value[4]}  |  {grid_value[5]}
+                    _____ _____ _____
+                      {grid_value[6]}  |  {grid_value[7]}  |  {grid_value[8]}
+                ''')
+                print(game_end)
+
+                time.sleep(1)
+                os.system("cls")
             break
